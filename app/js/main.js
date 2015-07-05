a7d48bbbe51bf8d47def04ec90c4ffbbfacd6e56
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-07-05 15:05:02 +0100

    Menu basic structure, styles and events added

--- app/js/main.js
+++ app/js/main.js
@@ -3,19 +3,20 @@ function Scene(){
     var self = this;
 
     self.init = function(){
-        setTimeout(function(){
-            var rocketFill = document.getElementById('rocket-fill'),
-                rocket = document.getElementById('Layer_1').querySelector('path');
+        //setTimeout(function(){
+        //    var rocketFill = document.getElementById('rocket-fill'),
+        //        rocket = document.getElementById('Layer_1').querySelector('path');
+        //
+        //    TweenMax.to(rocketFill, 0.6, {opacity:1,
+        //        ease: Circ.easeInOut,
+        //        onComplete: function(){
+        //            rocket.setAttribute('fill', '#3b4a49');
+        //
+        //        }
+        //    });
+        //}, 1.5);
 
-            TweenMax.to(rocketFill, 0.6, {opacity:1,
-                ease: Circ.easeInOut,
-                onComplete: function(){
-                    rocket.setAttribute('fill', '#3b4a49');
-                    self.initTitle();
-                }
-            });
-        }, 1.5);
-
+        self.initTitle();
     };
 
     self.initTitle = function(){
@@ -31,3 +32,29 @@ function Scene(){
 }
 
 var scene = new Scene();
+
+/*
+    Document ready
+ */
+document.addEventListener('DOMContentLoaded', function(){
+    var menuTrigger = document.getElementById('menu__trigger');
+    menuTrigger.addEventListener('click', function(e){
+        e.preventDefault();
+        var container = menuTrigger.parentNode,
+            menuContent = container.querySelector('.menu__content'),
+            className = 'open';
+        if (menuContent.classList) {
+            menuContent.classList.toggle(className);
+        } else {
+            var classes = menuContent.className.split(' ');
+            var existingIndex = classes.indexOf(className);
+
+            if (existingIndex >= 0)
+                classes.splice(existingIndex, 1);
+            else
+                classes.push(className);
+
+            menuContent.className = classes.join(' ');
+        }
+    });
+});
