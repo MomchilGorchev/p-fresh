bf1ddd66d73a52c7d2b2beb1b6f27c318cadd75e
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-08-07 01:09:33 +0100

    JS refactor and comments

--- app/js/main.js
+++ app/js/main.js
@@ -1,50 +1,51 @@
-
-function Scene(){
+/**
+ * Main title and CTA button
+ */
+(function Scene(){
     var self = this;
 
     self.init = function(){
-        //setTimeout(function(){
-        //    var rocketFill = document.getElementById('rocket-fill'),
-        //        rocket = document.getElementById('Layer_1').querySelector('path');
-        //
-        //    TweenMax.to(rocketFill, 0.6, {opacity:1,
-        //        ease: Circ.easeInOut,
-        //        onComplete: function(){
-        //            rocket.setAttribute('fill', '#3b4a49');
-        //
-        //        }
-        //    });
-        //}, 1.5);
 
-        self.initTitle();
-    };
-
-    self.initTitle = function(){
         var header = document.querySelector('header');
-        TweenMax.to(header, 1.8, {
-            //y: window.innerHeight / 3,
+        var ctaBtn = document.querySelector('#main-cta');
+        // Animate the header
+        TweenMax.to(header, 0.6, {
             opacity: 1,
-            ease: Circ.easeOut
+            ease: Power4.easeIn,
+            onComplete: function(){
+                // And the button a bit later
+                TweenMax.to(ctaBtn, 0.3, {
+                    y: -10,
+                    opacity: 1,
+                    ease: Power4.easeInOut
+                });
+            }
         });
     };
 
     self.init();
-}
+}());
 
-var scene = new Scene();
 
+/**
+ * Util function for proper animation sequence every time
+ * Uses CSS3 transitions
+ * @param menuTrigger - trigger
+ */
 function clickHandle(menuTrigger){
+    // Cache elements
     var container = menuTrigger.parentNode,
         menuContent = container.querySelector('.menu__content'),
         menuList = menuContent.querySelector('.menu__list'),
         openClassName = 'open', animClassName = 'spin';
+    // If nav is closed
     if(!hasClass(menuContent, openClassName)){
-
         toggleClass(menuTrigger, animClassName);
         toggleClass(menuContent, openClassName);
         setTimeout(function(){
             toggleClass(menuList, openClassName);
         }, 500);
+    // Else its open
     } else {
         toggleClass(menuTrigger, animClassName);
         toggleClass(menuList, openClassName);
@@ -53,26 +54,34 @@ function clickHandle(menuTrigger){
         }, 300);
     }
 }
-
+/**
+ * Navigation panel open/close
+ */
 function menuHandler(){
     var menuTrigger = document.getElementById('menu__trigger');
-
     menuTrigger.addEventListener('click', function(e){
         e.preventDefault();
         clickHandle(this);
     });
 }
 
+/**
+ * Main nav handler
+ */
 function menuClick(){
     var menuItems = document.querySelectorAll('.menu__list-item');
     var menuTrigger = document.getElementById('menu__trigger');
+    // Assign event-handlers to all links
     for(var i = 0; i < menuItems.length; i++){
         var item = menuItems[i];
         item.addEventListener('click', function(e){
             e.preventDefault();
+            // Get the target
             var href = this.querySelector('a').getAttribute('href');
             var DOMElement = document.querySelector(href);
+            // Call util function to handle open/close animation sequence
             clickHandle(menuTrigger);
+            // Scroll the window to the target
             TweenMax.to(window, 1,{
                 delay:0.6,
                 scrollTo: {
@@ -84,17 +93,19 @@ function menuClick(){
     }
 }
 
+/**
+ * Main CTA btn handler
+ */
 function launchSite(){
     var launch = document.querySelector('.main__trigger');
-
+    // Attach event
     launch.addEventListener('click', function(e){
         e.preventDefault();
         var target = document.querySelector('#projects');
         var scene = document.querySelector('#scene');
+        // Scale down the canvas for slicker animation
         toggleClass(scene, 'scaled');
-
-
-        console.log(target);
+        // Scroll the window el to the target location
         TweenMax.to(window, 1,{
             delay:0.6,
             scrollTo: {
@@ -102,13 +113,16 @@ function launchSite(){
             },
             ease: Power4.easeInOut,
             onComplete: function(){
+                // On complete scale back the canvas
                 toggleClass(scene, 'scaled');
             }
         });
     })
 }
 
-
+/**
+ * Overlay open/close handling
+ */
 function projectOverlay(){
     // Cache all necessary elements
     var triggers = document.querySelectorAll('.open-overlay');
@@ -117,30 +131,31 @@ function projectOverlay(){
     var closeTrigger = overlay.querySelector('.close');
     var body = document.querySelector('body');
 
-    // Assign handlers to the overlay triggers
+    // Assign event-handlers to the overlay triggers
     for(var i = 0; i < triggers.length; i ++){
         var current = triggers[i];
         current.addEventListener('click', function(e){
             e.preventDefault();
             // Clone related content to the overlay
-            var content = closest(this, 'figure').cloneNode(true).querySelector('.project__modal-details');
+            var content = closest(this, 'figure')
+                .cloneNode(true).querySelector('.project__modal-details');
             contentBox.appendChild(content);
             // Show the overlay
             toggleClass(overlay, 'open');
             toggleClass(body, 'overlay-open');
 
-            // Assign dynamically event to the 'Show-more' icon
+            // Assign dynamically event-handlers to the 'Show-more' icon
             var triggerId = content.getAttribute('data-trigger');
             document.getElementById(triggerId).addEventListener('click', function(e){
                 e.preventDefault();
                 // Get the target element
                 var target = document.getElementById(triggerId + '-details');
                 console.log(target.offsetTop);
-                // Very important, normal scroll on the window element won't work
+                // Important, normal scroll on the window element won't work
                 // because the overlay is fixed to take the whole screen
                 // The solution is to apply scroll on the overlay
                 TweenMax.to(overlay, 1,{
-                    delay:0.3,
+                    delay:0.2,
                     scrollTo: {
                         y: target.offsetTop
                     },
@@ -155,62 +170,19 @@ function projectOverlay(){
         });
     }
 
+    // Close btn logic
     closeTrigger.addEventListener('click', function(e){
         e.preventDefault();
+        // Little delay
         setTimeout(function(){
+            // Remove content
             var content = contentBox.querySelector('.project__modal-details');
             content.parentNode.removeChild(content);
+            // And close the overlay
             toggleClass(overlay, 'open');
             toggleClass(body, 'overlay-open');
         }, 300);
     });
-}
-
-function linkTransition(){
-
-    'use strict';
-
-    //var triggers = document.getElementsByClassName('overlay__show-more');
-
-
-
-
-    //for(var i = 0; i < triggers.length; i++){
-    //    (function assignHandle(i) {
-    //        console.log(triggers[i]);
-    //
-    //        triggers[i].addEventListener('click', function(e){
-    //
-    //            e.preventDefault();
-    //
-    //            console.log('assigned');
-    //
-    //            var target = this.getAttribute('data-href');
-    //            var DOMElement = document.querySelector('.'+ target);
-    //
-    //
-    //            TweenMax.to(window, 1,{
-    //                delay:0.3,
-    //                scrollTo: {
-    //                    y: DOMElement.offsetTop
-    //                },
-    //                ease: Power4.easeInOut
-    //            });
-    //
-    //        });
-    //
-    //    }(i));
-    //    console.log('i: '+ i);
-    //}
-
-
-
-        //triggers[i];
-        //    console.log(current.addEventListener);
-
-
-
-
 }
 
 /*
@@ -221,5 +193,4 @@ document.addEventListener('DOMContentLoaded', function(){
     menuHandler();
     menuClick();
     projectOverlay();
-    linkTransition();
 });
