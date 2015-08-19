25686f2e60ceba9ae911d1abda9a9ab9d0dea8ed
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-08-19 22:35:23 +0100

    Fixing footer canvas going out of the window width

--- app/js/main.js
+++ app/js/main.js
@@ -283,7 +283,7 @@ function formHandler(){
             console.log('yes!');
 
             var request = new XMLHttpRequest();
-            request.open('POST', 'http://localhost:3000/mail', true);
+            request.open('POST', 'http://178.62.25.62/mail', true);
             request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
 
             request.onload = function(res){
