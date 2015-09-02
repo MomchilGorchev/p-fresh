7f31b57edaa5f152a9f2534ae2d115ee9533603f
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-09-02 23:55:48 +0100

    Increasing alpha in a loop to achieve smoother animation

--- app/js/headerBG.js
+++ app/js/headerBG.js
@@ -11,10 +11,11 @@ function Space(){
     'use strict';
     var canvas = document.getElementById('welcome'),
         ctx = canvas.getContext('2d'),
+
         WIDTH = canvas.width = window.innerWidth,
         HEIGHT = canvas.height = window.innerHeight,
-        colors = ['204, 196, 123', '0, 214, 255', '179, 243, 255', '7, 108, 127', '143, 194, 204'],
 
+        colors = ['204, 196, 123', '0, 214, 255', '179, 243, 255', '7, 108, 127', '143, 194, 204'],
         centerX = WIDTH/2, centerY = HEIGHT/ 2,
         warpZ = 7,
         units = 300,
@@ -22,27 +23,31 @@ function Space(){
         Z = 0.12,
         M = Math,
         Rnd = M.random;
-    ctx.globalAlpha = 0.5; var alpha = 0;
+    ctx.globalAlpha = 0.5; var alpha = 0.05;
     var cx = (centerX - WIDTH / 2) + (WIDTH / 2),
         cy = (centerY - HEIGHT / 2) + (HEIGHT / 2);
 
-    //setInterval(function(){
-    //    if(units < 300){
-    //        units += 50;
-    //    }
-    //    generate();
-    //}, 5000);
+    // Increase the alpha every 700 ms for smooth animation
+    var alphaIncrease = setInterval(function(){
+        if(alpha < 1) {
+            alpha += 0.05;
+        } else {
+            clearInterval(alphaIncrease);
+        }
+    }, 700);
 
+    // Returns random array index
     function randomIndex(array){
         return array[Math.floor(Math.random() * array.length)];
     }
 
+    // Init loop
     function initSpace(){
         generate();
-
         loop();
     }
 
+    // Calculate position
     function resetBubble(a){
         a.x = (Rnd() * WIDTH - (WIDTH * 0.5)) * warpZ;
         a.y = (Rnd() * HEIGHT - (HEIGHT * 0.5)) * warpZ;
@@ -51,6 +56,7 @@ function Space(){
         a.py = 0;
     }
 
+    // First position
     function generate(){
         for (var i = 0, n; i < units; i++){
             n = {};
@@ -59,6 +65,7 @@ function Space(){
         }
     }
 
+    // Main animation sequence
     function loop(){
         ctx.fillStyle = 'rgba(0, 0, 0, 0.66)';
         ctx.fillRect(0, 0, WIDTH, HEIGHT);
@@ -70,14 +77,12 @@ function Space(){
                 yy = n.y / n.z,
                 radius = 1.0 / n.z * 1.5 + 1;      // size i.e. z
 
-                alpha = i < 100 ? i * 0.001 : 100;
-
             ctx.fillStyle = 'rgba(' + randomIndex(colors) + ', '+ alpha +')';
+            //console.log(ctx.fillStyle);
             if (n.px !== 0){
                 ctx.beginPath();
                 ctx.arc(xx + cx, yy + cy, radius, 0, Math.PI * 2, true);
                 ctx.arc(n.px + cx, n.py + cy, radius, 0, Math.PI * 2, true);
-                //ctx.arc(n.px - xx, n.py - yy, radius, 0, Math.PI * 2, true);
                 ctx.closePath();
                 ctx.fill();
             }
